1e247e0cc6c10d2fed287212d2d1999c2dfdcf3e
Simran kaur <simrankaur@magnaopussoftware.in>
2024-06-20 16:09:07 +0530

    add fevicon

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,17 +2,24 @@ const express = require("express");
 const app = express();
 const session = require("express-session");
 const morgan = require("morgan");
-
-
+const favicon = require('serve-favicon');
+const path = require('path'); // Make sure to import the 'path' module
 const cors = require("cors");
-app.use(cors());
 
-
+// Load environment variables from .env file
 require("dotenv").config();
 require("./config/db_connection");
+
 const port = process.env.PORT;
 const router = require("./routes/routers");
 
+// Serve the favicon
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+// Enable CORS
+app.use(cors());
+
+// Session setup
 app.use(
   session({
     secret: "A secret Key to sign the cookie",
@@ -20,14 +27,25 @@ app.use(
     resave: false,
   })
 );
+
+// Serve static files from 'public' and 'assets' directories
 app.use(express.static("public"));
 app.use(express.static('assets'));
 
+// Set view engine to EJS
 app.set("view-engine", "ejs");
+
+// Middleware for parsing JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// HTTP request logger
 app.use(morgan("tiny"));
+
+// Use the router for handling routes
 app.use(router);
+
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);
 });
